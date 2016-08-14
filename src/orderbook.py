5b2b6a71b2016f0738638469890f3ad3ac15e3b2
JRS <Jrsteeves@live.com>
2016-08-13 20:57:55 -0700

    bare bones

---
+++ src/orderbook.py
@@ -0,0 +1,22 @@
+# Maintains an updated order book for symbol 
+
+import poloniex
+
+
+class OrderBook(object):
+	def __init__(self):
+		pass
+	def startBook(self):
+		# Starts the catcher thread
+		pass
+	def stopBook(self):
+		# Ends the catcher thread
+		pass
+	def orderCatcher(self): 
+		# catches orders from ordercatcher
+		pass
+
+if __name__ == "__main__":
+	book = OrderBook()
+
+	
