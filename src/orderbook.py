c80a7e0cab4279db68404f428ecc1619154e3073
JRS <Jrsteeves@live.com>
2016-08-13 22:33:24 -0700

    added running multiple processes

--- src/orderbook.py
+++ src/orderbook.py
@@ -1,18 +1,34 @@
-from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
-from twisted.internet.defer import inlineCallbacks
 import time
 import poloniex
+from subprocess import Popen, PIPE
+from multiprocessing.dummy import Process as Thread
 
 class OrderBook(object):
 	def __init__(self):
-		order
+		self._receiverT = None
+		self._catchersP = {}
+
 	def startBook(self, symbol):
-		# Starts the catcher thread
-		pass
+		if not self._catchersP:
+			self._receiverT = Thread(target=self.orderReceiver); 
+			self._receiverT.daemon = True
+			self._receiverT.start()
+                	print('BOOK: receiver thread started')
+
+		self._catchersP[ symbol ] = Popen(["python", "ordercatcher.py", str(symbol) ], stdin=PIPE, stdout=PIPE, bufsize=1)
+                print('BOOK: {} process started'.format(symbol))
+
 	def stopBook(self, symbol):
-		# Ends the catcher thread
-		pass
-	def orderCatcher(self): 
+	
+		self._catchersP[ symbol ].terminate()	
+		self._catchersP[ symbol ].kill()
+		del self._catchersP[ symbol ]
+		print ('BOOK: {} process stopped'.format(symbol))
+		if not self._catchersP:
+			self._receiverT.join()
+			print('BOOK: reciever thread stopped')
+			
+	def orderReceiver(self): 
 		# catches orders from ordercatcher
 		pass
 
@@ -21,6 +37,6 @@ class OrderBook(object):
 if __name__ == "__main__":
 	book = OrderBook()
 	book.startBook("BTC_ETH")
-	time.sleep(10)
+	time.sleep(2)
 	book.stopBook("BTC_ETH")
 	
--- src/ordercatcher.py
+++ src/ordercatcher.py
@@ -1,13 +1,28 @@
 
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from twisted.internet.defer import inlineCallbacks
+import json
+import sys
+
+global symbol
+
+symbol = sys.argv[1]
+print symbol
+
+def onOrder(*args, **kw):
+	print(json.dumps(args))
 
 class OrderCatcher(ApplicationSession):
-        def __init__(self, symbol, config=None):
-                pass
+
+	def __init__(self,  config=None):
+        	ApplicationSession.__init__(self, config)
+              
+
         @inlineCallbacks
         def onJoin(self, details):
+		yield self.subscribe(onOrder, symbol)
+	 
 
-                def onOrder(*args, **kw):
-                        print json.dumps(args)
-                yield self.subscribe(onOrder, symbol)
+if __name__ == "__main__":
+	subscriber = ApplicationRunner(u"wss://api.poloniex.com:443", u"realm1")
+        subscriber.run( OrderCatcher )
